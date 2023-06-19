81d1f13bdc44331c921a7e473284a7a3f9a1390b
Perdigon <neil.james.perdigon@accenture.com>
2023-06-19 21:08:48 +0800

    added not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./component/Login";
-import Dashboard from "./component/Dashboard";
-import Category from "./component/category";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './component/Login';
+import Dashboard from './component/Dashboard';
+import Category from './component/category';
+import NotFound from './component/NotFound';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/createAccount" element={<Login />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/" element={<Dashboard />} />
+          <Route path='/createAccount' element={<Login />} />
+          <Route path='/category' element={<Category />} />
+          <Route path='/' element={<Dashboard />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
     </div>
